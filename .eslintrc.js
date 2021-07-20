8e1e8ee9d39d998b185afec027e9f8518ac4a92a
devganesh1998 <devganesh91@gmail.com>
2021-07-20 19:39:52 +0530

    eslint react setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,33 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'flowtype', 'import', 'jsx-a11y', 'react', 'react-hooks'],
     parserOptions: {
-      project: './tsconfig.json'
+        project: './tsconfig.json',
     },
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:flowtype/recommended',
+        'plugin:import/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:jsx-a11y/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
     ],
     rules: {
         'no-console': 1,
+        "react-hooks/exhaustive-deps": 1,
+        "react/prop-types": 0,
+    },
+    settings: {
+        react: {
+            pragma: 'React',
+            version: 'detect',
+        },
     },
 };
